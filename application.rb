a6f098f41d727055e8e8d1646810bc3b62b6b386
Cameron Carroll <ckcarroll4@gmail.com>
2015-10-09 14:56:46 -0700

    Add Warden, BCrypt, User Model & Auth Routes
    Added Warden/BCrypt gems
    Added Sinatra-flash gem
    Created user model, added auth setup and routes

--- application.rb
+++ application.rb
@@ -2,10 +2,95 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra/base'
 require 'sinatra/json'
+require 'sinatra/flash'
+require 'warden'
+require 'tilt/erb'
+require 'pry'
 
 require './model'
 
-class CammyCorner < Sinatra::Application
+LOGIN_FAIL_MSG = "Login or Password was incorrect."
+
+class NoteQueue < Sinatra::Application
+  enable :sessions
+  register Sinatra::Flash
+
+  use Warden::Manager do |config|
+    config.serialize_into_session{|user| user.id }
+    config.serialize_from_session{|id| User.get(id)}
+    config.scope_defaults :default,
+    strategies: [:password],
+    action: 'auth/unauthenticated'
+    config.failure_app = NoteQueue
+  end
+
+  Warden::Manager.before_failure do |env,opts|
+    env['REQUEST_METHOD'] = 'POST'
+  end
+
+  Warden::Strategies.add(:password) do
+    def valid?
+      params['login'] && params['password']
+    end
+
+    def authenticate!
+      user = User.first(login: params['login'])
+      if user.nil?
+        fail!(LOGIN_FAIL_MSG)
+      elsif user.authenticate(params['password'])
+        success!(user)
+      else
+        fail(LOGIN_FAIL_MSG)
+      end
+    end
+  end
+
+  #############################################################################
+  #############################################################################
+  # Auth Routes:
+
+  post '/auth/login' do
+    env['warden'].authenticate!
+    flash[:success] = env['warden'].message
+    redirect '/dash'
+  end
+
+  get '/auth/logout' do
+    # Apparently session won't clear without following inspection:
+    env['warden'].raw_session.inspect
+    env['warden'].logout
+    flash[:success] = 'Logged out!'
+    redirect '/'
+  end
+
+  post '/auth/unauthenticated' do
+    status 401
+    redirect '/'
+  end
+
+  post '/auth/create' do
+    login = params[:login]
+    password = params[:password]
+    confirmation = params[:confirmation]
+
+    if User.first(:login => login)
+      # user exists already
+      redirect '/register'
+    else
+      if password == confirmation
+        user = User.create(:login => login, :password => password)
+        binding.pry
+        env['warden'].set_user(user)
+        redirect '/dash'
+      else
+        # passwords didn't match
+        redirect '/register'
+      end
+    end
+  end
+  #############################################################################
+  #############################################################################
+  # Program API Routes:
 
   post '/entry' do
     stamp = Time.new
@@ -24,7 +109,12 @@ class CammyCorner < Sinatra::Application
     json entries
   end
 
+  #############################################################################
+  #############################################################################
+  # Website Routes:
+
   get '/' do
+    bump_logged_in
     erb :index
   end
 
@@ -32,9 +122,26 @@ class CammyCorner < Sinatra::Application
     erb :create_account
   end
 
-  post 'auth/login' do
+
+
+  #############################################################################
+  #############################################################################
+
+  private
+
+  def logged_in?
+    env['warden'].user
   end
 
-  post 'auth/create' do
+  def logged_out?
+    !logged_in?
+  end
+
+  def bump_logged_in
+    redirect '/dash' if logged_in?
+  end
+
+  def bump_logged_out
+    redirect '/' if logged_out?
   end
 end
--- model.rb
+++ model.rb
@@ -1,6 +1,7 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 require 'dm-serializer'
+require 'bcrypt'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/camdev')
 
@@ -10,6 +11,23 @@ class Entry
   property :id, Serial
   property :datestamp, DateTime
   property :message, Text, :lazy => false
+
+  belongs_to :user
+end
+
+class User
+  include DataMapper::Resource
+  include BCrypt
+
+  property :id, Serial
+  property :login, String, :length => 3..50
+  property :password, BCryptHash
+
+  has n, :entries
+
+  def authenticate(attempted_password)
+    self.password == attempted_password
+  end
 end
 
 DataMapper.finalize
